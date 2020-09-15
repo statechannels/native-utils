b8c970318646a8fb024b87b9a8f8a79804ee4873
Andrew Stewart <andrew.gord.stewart@gmail.com>
2020-09-14 19:58:04 -0700

    Copy JSON neon example
    ```
    encode-state is 📦 v0.1.0 via ⬢ v12.16.3 on ☁️  us-west-1
    ❯ node lib/index.js
    Hello, john!
    ```

---
+++ native/src/lib.rs
@@ -0,0 +1,19 @@
+use neon::register_module;
+use neon_serde::export;
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct User {
+  name: String,
+  age: u16,
+}
+
+export! {
+    fn sayHello(name: String) -> String {
+        format!("Hello, {}!", name)
+    }
+
+    fn greet(user: User) -> String {
+        format!("{} is {} years old", user.name, user.age)
+    }
+}

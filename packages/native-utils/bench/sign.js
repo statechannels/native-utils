6af3232bd75981f3b04e33d8b374745f17266b77
geoknee <georgeknee@googlemail.com>
2020-12-01 19:07:53 +0000

    test: rip out server-wallet-based benchmarks

--- packages/native-utils/bench/sign.js
+++ packages/native-utils/bench/sign.js
@@ -1,6 +1,4 @@
 const benny = require('benny')
-const serverWallet = require('@statechannels/server-wallet/lib/src/utilities/signatures')
-const nitro = require('@statechannels/nitro-protocol')
 const wasm = require('@statechannels/wasm-utils')
 const native = require('..')
 
@@ -32,44 +30,9 @@ const DEFAULT_STATE = {
   appData: '0x0000000000000000000000000000000000000000000000000000000000000000',
 }
 
-const WALLET_CORE_DEFAULT_STATE = {
-  turnNum: DEFAULT_STATE.turnNum,
-  isFinal: DEFAULT_STATE.isFinal,
-  chainId: DEFAULT_STATE.channel.chainId,
-  channelNonce: DEFAULT_STATE.channel.channelNonce,
-  participants: DEFAULT_STATE.channel.participants.map(p => ({
-    participantId: p,
-    destination: p,
-    signingAddress: p,
-  })),
-  challengeDuration: DEFAULT_STATE.challengeDuration,
-  outcome: {
-    type: 'SimpleAllocation',
-    assetHolderAddress: OUTCOME[0].assetHolderAddress,
-    allocationItems: OUTCOME[0].allocationItems.map(ai => ({
-      destination: ai.destination,
-      amount: ai.amount,
-    })),
-  },
-  appData: DEFAULT_STATE.appData,
-  appDefinition: DEFAULT_STATE.appDefinition,
-}
-
 module.exports = () =>
   benny.suite(
     'State signing',
-
-    benny.add('fastSignState (nitro, wasm)', async () => {
-      await serverWallet.fastSignState(
-        {
-          ...WALLET_CORE_DEFAULT_STATE,
-          // We include the hashing here, because `signState` does it internally;
-          // it wouldn't be fair to hash the state once outside this benchmark
-          stateHash: nitro.hashState(DEFAULT_STATE),
-        },
-        PRIVATE_KEY,
-      )
-    }),
 
     benny.add('signState (native)', () => {
       native.signState(DEFAULT_STATE, PRIVATE_KEY)
